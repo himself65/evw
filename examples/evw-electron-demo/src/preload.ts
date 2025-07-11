a006a61aba39ab49ef4bb847abd8e6243f5dd9eb
Alex Yang <himself65@outlook.com>
2025-07-10 21:07:00 -0700

    fix: code

--- examples/evw-electron-demo/src/preload.ts
+++ examples/evw-electron-demo/src/preload.ts
@@ -1,8 +1,9 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import { startEvent } from "./events";
 import { initEventChannel } from "evw/electron/preload";
 
 const initPromise = initEventChannel();
 
-await initPromise;
+initPromise.then(() => {
+  console.log("preload init");
+});
